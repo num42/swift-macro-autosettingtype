071eb8c81e21cb9ac2802f82980443439ea32198
patrickavs <patrick-rc-pfeil@gmx.de>
2025-09-30 09:52:45 +0200

    format using `swift format --recursive --in-place .`

--- Tests/AutoSettingTypeTests/AutoSettingTypeTests.swift
+++ Tests/AutoSettingTypeTests/AutoSettingTypeTests.swift
@@ -10,25 +10,25 @@ import XCTest
     "AutoSettingType": AutoSettingTypeMacro.self
   ]
 
-final class AutoSettingTypeTests: XCTestCase {
-  func testStandardType() throws {
-    testMacro(macros: testMacros)
-  }
+  final class AutoSettingTypeTests: XCTestCase {
+    func testStandardType() throws {
+      testMacro(macros: testMacros)
+    }
 
-  func testSimpleGenericType() throws {
-    testMacro(macros: testMacros)
-  }
+    func testSimpleGenericType() throws {
+      testMacro(macros: testMacros)
+    }
 
-  func testAdvancedGenericType() throws {
-    testMacro(macros: testMacros)
-  }
+    func testAdvancedGenericType() throws {
+      testMacro(macros: testMacros)
+    }
 
-  func testArray() throws {
-    testMacro(macros: testMacros)
-  }
+    func testArray() throws {
+      testMacro(macros: testMacros)
+    }
 
-  func testArrayWithProperty() throws {
-    testMacro(macros: testMacros)
+    func testArrayWithProperty() throws {
+      testMacro(macros: testMacros)
+    }
   }
-}
 #endif
